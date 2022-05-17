2358da59cc1f3df0733d6c393e0e24d0505d4c08
Shane Dorsey <scdorsey22@gmail.com>
2022-05-17 09:58:24 -0700

    commit tree f3b5d7d6f00212477300de056e210a492134505c

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(customer) {
+    return Math.abs(customer -42)
+} 
+    
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks)*264 
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+
+}
+
+function calculatesFarePrices(start, destination) {
+
+}
