aa46a201d9c95f92b02606c0039a797005776c0b
Shane Dorsey <scdorsey22@gmail.com>
2022-05-18 10:49:41 -0700

    commit tree 1cc0650e624cd719d4e1438d8788ad7d6c2fe93a

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ function distanceFromHqInFeet(blocks) {
 
 
 function distanceTravelledInFeet(start, destination) {
-
+    return Math.abs((start -destination)*264)
+        
 }
 
-function calculatesFarePrices(start, destination) {
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400)
+    return 0
+    else (400 <= distanceTravelledInFeet(start, destination) < 2000)
+    return ((distanceTravelledInFeet(start, destination)-400) * .02)
 
 }
